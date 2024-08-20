55c6969da2c16fc9fab601a012fdab65562cc485
Huynh Thanh Thao <huynhthanhthaoctu@gmail.com>
2024-08-20 17:54:43 +0700

    fix / cors

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { AppModule } from './app.module';
-import { json, static as static_ } from 'express';
-import {
-  BadRequestException,
-  HttpStatus,
-  ValidationPipe,
-} from '@nestjs/common';
-import { PrismaExceptionFilter, errorFormatter } from 'utils/ApiErrors';
-import { ValidationError } from 'class-validator';
-import { TransformInterceptor } from 'utils/ApiResponse';
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { AppModule } from "./app.module";
+import { json, static as static_ } from "express";
+import { BadRequestException, HttpStatus, ValidationPipe } from "@nestjs/common";
+import { PrismaExceptionFilter, errorFormatter } from "utils/ApiErrors";
+import { ValidationError } from "class-validator";
+import { TransformInterceptor } from "utils/ApiResponse";
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
     const cfgService = app.get(ConfigService);
 
-    app.use('/uploads', static_('uploads'));
-    app.use(json({ limit: '200mb' }));
+    app.enableCors({ origin: true });
+    app.use("/uploads", static_("uploads"));
+    app.use(json({ limit: "200mb" }));
     app.useGlobalFilters(new PrismaExceptionFilter());
     app.useGlobalInterceptors(new TransformInterceptor());
-    app.setGlobalPrefix('api');
-    app.enableCors();
+    app.setGlobalPrefix("api");
 
     app.useGlobalPipes(
       new ValidationPipe({
@@ -33,14 +29,14 @@ async function bootstrap() {
           const formattedErrors = errorFormatter(errors);
           return new BadRequestException({
             statusCode: HttpStatus.BAD_REQUEST,
-            message: 'Validation failed!',
+            message: "Validation failed!",
             errors: formattedErrors,
           });
         },
       }),
     );
 
-    await app.listen(cfgService.get<number>('PORT'));
+    await app.listen(cfgService.get<number>("PORT"));
   } catch (error) {
     console.log(error);
   }
