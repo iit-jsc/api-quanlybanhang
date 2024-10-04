ad590e3fe617d8859d0aa9b6b0b457631b1cfed7
Thanh Thao <huynhthanhthaoctu@gmail.com>
2024-10-04 08:33:34 +0700

    fix / add include area

--- src/customer-request/customer-request.service.ts
+++ src/customer-request/customer-request.service.ts
@@ -120,7 +120,11 @@ export class CustomerRequestService {
             }
           },
         },
-        table: true
+        table: {
+          select: {
+            area: true,
+          }
+        }
       }
     });
   }
