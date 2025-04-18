ad0540b431f1bf533f9c75b99984fd3bc2bb1657
Thanh Thao <huynhthanhthaoctu@gmail.com>
2025-04-18 15:57:13 +0700

    fix / response payment method

--- responses/payment-method.response.ts
+++ responses/payment-method.response.ts
@@ -4,6 +4,7 @@ export const paymentMethodSelect: Prisma.PaymentMethodSelect = {
   id: true,
   bankCode: true,
   bankName: true,
+  active: true,
   photoURL: true,
   representative: true,
   type: true,
