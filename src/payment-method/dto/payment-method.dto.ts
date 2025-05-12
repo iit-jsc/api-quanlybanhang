75c2c6395d79edaf8e35f6120a6f491b6933eacd
Thanh Thao <huynhthanhthaoctu@gmail.com>
2025-05-12 09:48:51 +0700

    fix / validate payment method

--- src/payment-method/dto/payment-method.dto.ts
+++ src/payment-method/dto/payment-method.dto.ts
@@ -1,18 +1,12 @@
 import { PartialType } from '@nestjs/swagger'
 import { PaymentMethodType } from '@prisma/client'
-import { Transform, TransformFnParams } from 'class-transformer'
-import { IsEnum, IsOptional, Matches } from 'class-validator'
+import { Transform } from 'class-transformer'
+import { IsEnum, IsOptional } from 'class-validator'
 import { FindManyDto } from 'utils/Common.dto'
 
 export class CreatePaymentMethodDto {
-  @IsOptional()
-  @Transform(({ value }: TransformFnParams) => value?.trim())
-  bankName: string
-
-  @IsOptional()
-  @Transform(({ value }: TransformFnParams) => value?.trim())
-  @Matches(/^\d+$/)
-  bankCode: string
+  bankName?: string
+  bankCode?: string
 
   @IsOptional()
   @IsEnum(PaymentMethodType)
