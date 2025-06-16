bf558a17e86569023e1ab869e02a6a19e27687b1
huynhthanhthao <huynhthanhthaoctu@gmail.com>
2025-06-16 09:46:59 +0700

    logs: view ipn callback

--- src/vnpay/vnpay.service.ts
+++ src/vnpay/vnpay.service.ts
@@ -352,6 +352,8 @@ export class VNPayService {
   async vnPayIPNCallback(ipnDto: VNPayIPNDto) {
     const { txnId, payDate, responseCode, checksum } = ipnDto
 
+    console.log('run IPN Callback with data:', ipnDto)
+
     // 1. Lấy transaction theo txnId
     const transaction = await this.prisma.vNPayTransaction.findUnique({
       where: { vnpTxnRef: txnId },
